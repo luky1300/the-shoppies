6ebc751dc4b23eef35e41f8e68f31d7e2ab006e1
luky1300 <58443403+luky1300@users.noreply.github.com>
2020-09-03 19:53:15 -0400

    Updates

---
+++ precache-manifest.c8978bc24de89963e77eeb98d71c5dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7d5a36b48b6cfe13d2eabe7cb93ea7",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "6071c50667c77052f629",
+    "url": "/the-shoppies/static/css/main.8fd0d8f3.chunk.css"
+  },
+  {
+    "revision": "0e99e2629f3375b08219",
+    "url": "/the-shoppies/static/js/2.d316c857.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/the-shoppies/static/js/2.d316c857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6071c50667c77052f629",
+    "url": "/the-shoppies/static/js/main.3e00818a.chunk.js"
+  },
+  {
+    "revision": "3028237cd38fe8ab6725",
+    "url": "/the-shoppies/static/js/runtime-main.7a5fa803.js"
+  }
+]);
